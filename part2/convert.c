459bb6053e170abe2868a546b5c691156ccce0bb
Nina Baculinao <nb2406@columbia.edu>
2015-02-01 21:09:23 -0500

    feat(convert.c): convert signed decimal to unsigned and hex

---
+++ part2/convert.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int bin(int a){
+
+}
+
+
+int main(){
+    int x, hex;
+    scanf("%d", &x);
+    
+    printf("signed dec:\t%d\n", x);
+    printf("unsigned dec:\t%u\n", x);
+    printf("hex:\t\t%x\n", x);
+    
+    printf("binary:\t");
+
+    return 0;
+}
