bf729a3d93c9fa141a54747bf0aee307b1f389f1
Nina Baculinao <nb2406@columbia.edu>
2015-02-24 13:21:50 -0500

    feat(revecho.c): check if 'dude' is passed into command line args

--- lab3/part2/revecho.c
+++ lab3/part2/revecho.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "mylist.h"
 
 static void printString(void *p) {
@@ -18,6 +19,16 @@ int main(int argc, char **argv) {
     for (i = 1; i < argc; i++)
         addFront(&list, argv[i]); // call on all strings, adding next to front
     traverseList(&list, &printString); // traverse list and print strings
+
+    struct Node *node;
+    node = findNode(&list, "dude", (int (*)(const void *, const void *))&strcmp);
+    // initial error: expected ‘int (*)(const void *, const void *)’
+    // but argument is of type ‘int (*)(const char *, const char *)’
+    // therefore we cast strcmp to func pointer with void * input
+    if (node == NULL)
+        printf("\ndude not found\n");
+    else
+        printf("\ndude found\n");
 
     removeAllNodes(&list);
     return 0;
