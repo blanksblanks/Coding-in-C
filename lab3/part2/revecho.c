fdb148b351efaa0c7141aeedd24a8e094bf46fff
Nina Baculinao <nb2406@columbia.edu>
2015-02-24 12:43:20 -0500

    add revecho class that takes command line args, passes them into linked list and returns backwards

---
+++ lab3/part2/revecho.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "mylist.h"
+
+static void printString(void *p) {
+    printf("%s\n", (char *)p);
+    // if I add another * in front it complains I'm passing an int - why?
+}
+
+int main(int argc, char **argv) {
+
+    // initialize list
+    struct List list;
+    initList(&list);
+    
+    // pass arg strings into list without duplicating them
+    int i;
+    for (i = 1; i < argc; i++)
+        addFront(&list, argv[i]); // call on all strings, adding next to front
+    traverseList(&list, &printString); // traverse list and print strings
+
+    return 0;
+
+}
